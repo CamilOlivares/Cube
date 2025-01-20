1161ded8778c574195d5779cc9fe0ea8c1936434
Camilo Olivares <140129147+CamilOlivares@users.noreply.github.com>
2025-01-20 12:39:24 -0300

    Create cube.php

---
+++ cube.php
@@ -0,0 +1,10 @@
+<div class="cube-container">
+  <div class="cube">
+    <div class="face front">PHP</div>
+    <div class="face back">CSS</div>
+    <div class="face right">HTML</div>
+    <div class="face left">JS</div>
+    <div class="face top">Wordpress</div>
+    <div class="face bottom">Github</div>
+  </div>
+</div>
